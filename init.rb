79af1d671179bd6e9b7ac01fb50e58397a0185ba
森林ソフト <kimoto@forest-soft.net>
2021-06-15 22:58:27 +0900

    初期版作成

---
+++ init.rb
@@ -0,0 +1,14 @@
+﻿require 'redmine'
+
+require_dependency 'notify_login_hook_listener'
+
+Rails.application.config.i18n.load_path += Dir["#{File.dirname(__FILE__)}/config/locales/*.{rb,yml}"]
+
+Redmine::Plugin.register :redmine_notify_login do
+  name 'Redmine Notify Login plugin'
+  author 'Forest Soft'
+  description 'ログイン通知メールを送信できるようになります。'
+  version '1.0.0'
+  url 'https://github.com/forest-soft/redmine_notify_login'
+  author_url 'http://forest-soft.net'
+end
---
+++ lib/notify_login_hook_listener.rb
@@ -0,0 +1,13 @@
+class PollsHookListener < Redmine::Hook::ViewListener
+	def controller_account_success_authentication_after(context)
+		::Mailer.security_notification(
+			context[:user],
+			User.current,
+			{
+				title: :label_my_account,
+				message: 'notify_login_mail_body',
+				url: {controller: 'my', action: 'account'}
+			}
+		).deliver
+	end
+end
